4fa2813e3454e28cfd72dc4c547a46d5372b6101
BaalEvan <baalevan@outlook.com>
2023-08-26 19:12:26 +0200

    Initial Commit

---
+++ UnitySourceTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors.Assets.Plugins.LocalOnly.SourceTemplates
+{
+    using JetBrains.Annotations;
+
+    public class UnitySourceTemplate
+    {
+     
+            // [SourceTemplate]
+            // public static void sf(this string str)
+            // {
+            //     if (String.IsNullOrEmpty(str))
+            //     {
+            //         //$ $END$
+            //     }
+            // }
+        }
+}
